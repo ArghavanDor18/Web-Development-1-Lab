c6c52c84d24128fe9a61babac11ae7909c1457b0
Arghavan Dor <97262547+ArghavanDor18@users.noreply.github.com>
2022-02-11 15:49:08 -0800

    index.html

--- jquery.js
+++ jquery.js
@@ -1,14 +1,15 @@
 function calculate_area() {
+    console.log("calculate_area()")
     //alert(jQuery("body").html;
 
     //alert(jQuery("#x").val());
-    r = parseInt(jQuery("#x").val()) ;
+    r = parseInt(jQuery("#x").value()) ;
     //console.log( r * r * 22/7)
     jQuery("#p1").html(r * r * 22/7)
 }
 
 function setup() {
-    
+    console.log("setup() got called")
     jQuery("#calc").click(calculate_area);
 }
 
